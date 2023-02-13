6229522de6e4d1677679c5882e5d09f50a257467
vladsigopka <vlad.tereshhenko.00@mail.ru>
2023-02-13 17:03:21 +0700

    5 practical

--- js/main.js
+++ js/main.js
@@ -7,5 +7,34 @@ let app = new Vue({
         inStock: true,
         inventory: 100,
         onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            }
+        ],
+
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+        removeToCart(){
+            this.cart -= 1
+        }
+
     }
+
 })
